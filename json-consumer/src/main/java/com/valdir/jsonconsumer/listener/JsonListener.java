b46a259a52b1712689b60e1e03121ef6c2a1db49
Wagner Silva <190129867@aluno.unb.br>
2022-09-12 13:08:25 -0300

    Subindo o projeto.

---
+++ json-consumer/src/main/java/com/valdir/jsonconsumer/listener/JsonListener.java
@@ -0,0 +1,45 @@
+package com.valdir.jsonconsumer.listener;
+
+import com.valdir.jsonconsumer.model.Payment;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import static java.lang.Thread.sleep;
+
+@RequiredArgsConstructor
+@Log4j2
+@Component
+public class JsonListener {
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "create-group", containerFactory = "jsonContainerFactory")
+    public void antiFraude(@Payload Payment payment) {
+
+        log.info("Recebi o pagamento {}", payment.toString());
+        log.info("Validando fraude ...");
+        sleep(2000);
+        log.info("Compra aprovada ...");
+        sleep(2000);
+    }
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "pdf-group", containerFactory = "jsonContainerFactory")
+    public void pdfGenerator(@Payload Payment payment) {
+        log.info("Gerando pdf do produto de id {}", payment.getId());
+        sleep(3000);
+    }
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "email-group", containerFactory = "jsonContainerFactory")
+    public void sendEmail() {
+        log.info("Enviando email de confirmacao ...");
+        sleep(2000);
+
+        log.info("Compra aprovada ...");
+    }
+
+}
---
+++ payment-service/src/main/java/com/valdir/paymentservice/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.valdir.paymentservice.service;
+
+import com.valdir.paymentservice.model.Payment;
+
+public interface PaymentService {
+
+    void sendPayment(Payment payment);
+
+}
